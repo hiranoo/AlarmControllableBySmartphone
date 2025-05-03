a5c5a1b1f9d5b20219f39e48b87d364af4bae3e3
hiranoo <opfure62@gmail.com>
2025-05-03 19:33:31 +0900

    create bluetooth connection classes

--- AlarmControllableBySmartphone/app/src/main/java/com/example/alarmcontrollablebysmartphone/MainActivity.kt
+++ AlarmControllableBySmartphone/app/src/main/java/com/example/alarmcontrollablebysmartphone/MainActivity.kt
@@ -34,7 +34,10 @@ class MainActivity : ComponentActivity() {
     lateinit var bluetoothManager: BluetoothManager
     lateinit var bluetoothAdapter: BluetoothAdapter
     var bluetoothDevice: BluetoothDevice? = null
+    var connectThread: ConnectThread? = null
+    var connectedThread: ConnectedThread? = null
 
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBluetooth()
@@ -78,6 +81,82 @@ class MainActivity : ComponentActivity() {
                     // TODO
                     return
                 }
+            }
+        }
+    }
+
+    fun manageMyConnectedSocket(socket: BluetoothSocket) {
+        connectedThread = ConnectedThread(socket)
+    }
+
+    @SuppressLint("MissingPermission")
+    inner class ConnectThread(device: BluetoothDevice) : Thread() {
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(device.uuids[0].uuid)
+        }
+
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter.cancelDiscovery()
+            mmSocket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                socket.connect()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+                manageMyConnectedSocket(socket)
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        @OptIn(UnstableApi::class)
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+                androidx.media3.common.util.Log.e(TAG, "Could not close the client socket", e)
+            }
+        }
+    }
+
+    inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+
+        private val mmInStream: InputStream = mmSocket.inputStream
+        private val mmOutStream: OutputStream = mmSocket.outputStream
+        private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
+
+        @OptIn(UnstableApi::class)
+        override fun run() {
+            val reader = BufferedReader(mmInStream.reader())
+            var content: String
+            try {
+                content = reader.readText()
+            } finally {
+                reader.close()
+            }
+            androidx.media3.common.util.Log.d(TAG, content)
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        @OptIn(UnstableApi::class)
+        fun write(bytes: ByteArray) {
+            try {
+                androidx.media3.common.util.Log.d("abcdef", "start writing")
+                mmOutStream.write(bytes)
+            } catch (e: IOException) {
+                androidx.media3.common.util.Log.e(TAG, "Error occurred when sending data", e)
+                return
+            }
+        }
+
+        // Call this method from the main activity to shut down the connection.
+        @OptIn(UnstableApi::class)
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                androidx.media3.common.util.Log.e(TAG, "Could not close the connect socket", e)
             }
         }
     }
